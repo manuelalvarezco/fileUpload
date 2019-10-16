8cd7f202f4d68e7445eea626078a7f559f9a05df
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2019-10-16 13:57:38 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CargaImagenesService } from './services/carga-imagenes.service';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotosComponent,
+    CargaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
-  providers: [],
+  providers: [
+    CargaImagenesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/file-items.ts
@@ -0,0 +1,15 @@
+export class FileItem{
+    public archivo:File;
+    public nombreArchivo:string;
+    public url:string;
+    public subiendo:boolean;
+    public progreso:number;
+
+    constructor(archivo:File){
+
+        this.archivo = archivo;
+        this.nombreArchivo = archivo.name;
+        this.subiendo = false;
+        this.progreso = 0;
+    }
+}
